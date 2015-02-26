a22e24a87b15ce0314bca5e4ad6710db633e1b2f
Alejandro <ale90nb@gmail.com>
2015-02-26 11:13:33 +0100

    Cambio de rumbo

--- script-alta.py
+++ script-alta.py
@@ -7,42 +7,49 @@ from random import choice
 import string
 
 
-#pedimos el nombre y el dominio que se usara
+#pedimos nombre y dominio
 
-nombre = (sys.argv[1])
+usuario = (sys.argv[1])
 dominio = (sys.argv[2])
 
 
 #siguiente paso: conectarnos a la bbdd
 
-conexion = MySQLdb.connect(host = "localhost", user = "root", passwd = "usuario", db = "hosting")
-db = MySQLdb.connect(host="localhost", # your host, usually localhost
+conexion = MySQLdb.connect(host="localhost", # your host, usually localhost
                      user="hosting", # your username
                       passwd="hosting", # your password
                       db="hosting") # name of the data base
+
 cursor = conexion.cursor()
 
+consulta_usuario = 'select username from usuarios where username = '%s';' %usuario
+
+cursor.execute(consulta_usuario)
+
+consulta_usuario=cursor.fetchone()
+
+if consulta_usuario != None:
+	print "Ese usuario ya está registrado"
+	sys.exit
+else:
+	consulta_dominio = "select dominio from usuarios where dominio = '%s';" %dominio
+	cursor.execute(consulta_dominio)
+	consulta_dominio=cursor.fetchone()
+
+	if consulta_dominio != None:
+		print "Ese dominio ya existe"
 
 
 
 
 
-Script modelo:
 
-import MySQLdb
+if os.path.isdir("/var/www/%s" % usuario) = False
+and os.path.isfile("/etc/apache2/sites-available/%s" %dominio) = False:
+	os.system("mkdir /var/www/%s" %usuario)
+else:
+	print "El usuario o el dominio ya existen, introduzca otro usuario y dominio por favor." % (usuario)
+	exit()
 
-db = MySQLdb.connect(host="192.168.100.1", # your host, usually localhost
-                     user="conexion", # your username
-                      passwd="conexion", # your password
-                      db="conexion") # name of the data base
 
-cur = db.cursor()
 
-sql = 'select * from maillot'
-
-cur.execute(sql)
-
-resultado=cur.fetchall()
-
-for registro in resultado:
-        print "Codigo: ",registro[0],"Tipo: ",registro[1],"Color: ",registro[2],"Premio: ",registro[3]
