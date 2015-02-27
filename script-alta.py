f60c537348c35d7267f2759e6b8dc504c0bd1448
Alejandro <ale90nb@gmail.com>
2015-02-27 12:50:26 +0100

    Añadir plantillas

--- script-alta.py
+++ script-alta.py
@@ -9,7 +9,7 @@ import string
 
 #pedimos nombre y dominio
 
-usuario = (sys.argv[1])
+nombre = (sys.argv[1])
 dominio = (sys.argv[2])
 
 
@@ -22,7 +22,7 @@ conexion = MySQLdb.connect(host="localhost", # your host, usually localhost
 
 cursor = conexion.cursor()
 
-consulta_usuario = 'select username from usuarios where username = '%s';' %usuario
+consulta_usuario = 'select username from usuarios where username = '%s';' %nombre
 
 cursor.execute(consulta_usuario)
 
@@ -38,18 +38,83 @@ else:
 
 	if consulta_dominio != None:
 		print "Ese dominio ya existe"
+		sys.exit
+	else:
+		os.system("mkdir /srv/www/%s" %nombre)
+		os.system("cp /var/www/index.html /srv/www/%s" %nombre)
+        fichero = "/home/ale/templates/vhost"
+        r_host = open(fichero,"r")
+        w_host = open('/etc/apache2/sites-available'+dominio,"w")
+        buffer_fichero = r.host.read()
+        read_buffer_1 = buffer_fichero.replace("**usuario**", nombre)
+        read_buffer_2 = read_buffer_1.replace("**dominio**", dominio)
+        w_host.write(read_buffer_2)
+        r_host.close()
+        w_host.close()
+        os.system("a2ensite "+dominio)
+        os.system("service apache2 restart")
 
 
 
+        #generamos una contrasenya aleatoria
+        def GenPasswd(n):
+            return ''.join([choice(string.letters + string.digits) for i in range(n)])
+            contrasenna=GenPasswd(8)
+            print"esta es tu contrasenna para el usuario %s ftp:"%nombre, contrasenna
+
+			#insertamos el usuario en mysql
+            consultauid="select max(uid) from usuarios;"
+            cursor.execute(consultauid)
+            consulta_uid = cursor.fetchone()
+            #si la tabla está vacía introduce el 5001
+            if consulta_uid[0] == None:
+                conuid = "5001"
+            else:
+                conuid = str(int(consulta_uid[0])+1)
+            usermysql="insert into usuarios values('"+ nombre+"'," +"PASSWORD('"+contrasenna+"'),"+conuid+",33,'/srv/www/"+nombre+"',"+"'/bin/false',"+"1,'"+dominio+"');"
+            cursor.execute(usermysql)
+            conexion.commit()
+            #cambiamos los permisos del directorio para que el usuario pueda subir datos y acceder apache
+            chown="chown "+conuid+":www-data /srv/www/"+nombre+" -R"
+            chmod="chmod 770 -R /srv/www/"+nombre
+            os.system(chown)
+            os.system(chmod)
 
 
+            #creamos una base de datos con una contraseña para el usuario
+            cursor.execute("create database my"+nombre+";")
+            passwdMysql=GenPasswd(8)
+            #print "pass mysql "+passwdMysql
+            grantPerm="grant all on my"+nombre+".* to 'my"+nombre+"'@'localhost' identified by '"+passwdMysql+"';"
+            print "la contraseña del musuario my"+nombre+" es: " + passwdMysql
+            cursor.execute(grantPerm)
+            conexion.commit()
 
-if os.path.isdir("/var/www/%s" % usuario) = False
-and os.path.isfile("/etc/apache2/sites-available/%s" %dominio) = False:
-	os.system("mkdir /var/www/%s" %usuario)
-else:
-	print "El usuario o el dominio ya existen, introduzca otro usuario y dominio por favor." % (usuario)
-	exit()
+
+            #creamos la zona dns para el usuario que se esta creando
+            ficherodominio="/home/ale/templates/db.plantilla"
+            domain=open(ficherodominio, "r")
+            filew = open('/var/cache/bind/db.'+dominio, "w")
+            buff = domain.read()
+            variable1='**dominio**'
+            rbuff = buff.replace(variable1, dominio)
+            filew.write(rbuff)
+            domain.close()
+            filew.close()
 
 
+            #creamos la nueva zona
+			fichzona="/home/ale/templates/zona"
+            zonadom=open(fichzona,"r")
+            zonabuff = zonadom.read()
+            char1='**dominio**'
+            cambio = zonabuff.replace(char1, dominio)
+            g=open("/etc/bind/named.conf.local","a")
+            g.write(cambio)
+            g.close()
+            zonadom.close()
 
+
+            #reiniciamos el servidor dns
+            rebootdns = os.system("service bind9 restart")
+            print "El usuario se ha creado correctamente"
